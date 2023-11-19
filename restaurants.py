d58abc511686c784ba20d0f109cc573fc4124b24
Niko <101094386+nikokurki@users.noreply.github.com>
2023-11-19 23:44:57 +0200

    Add files via upload

---
+++ restaurants.py
@@ -0,0 +1,26 @@
+from db import db
+from sqlalchemy.sql import text
+
+import users
+
+
+def get_list():
+    sql = text("SELECT restaurant_id, name, longitude, latitude FROM restaurants ORDER BY restaurant_id")
+    result = db.session.execute(sql)
+    return result.fetchall()
+
+def get_restaurantdata(id):
+    sql = text("SELECT R.name, I.description, I.open_hours FROM restaurant R, info I WHERE R.restaurant_id = id AND I.restaurant_id = id")
+    result = db.session.execute(sql)
+    return result.fetchall()
+
+
+
+def add_restaurant(name,longitude,latitude):
+    user_id = users.user_id()
+    if user_id == 0:
+        return False
+    sql = text("INSERT INTO restaurants (name, longitude, latitude) VALUES (:name, :longitude, :latitude)")
+    db.session.execute(sql, {"name":name, "longitude":longitude, "latitude":latitude })
+    db.session.commit()
+    return True
---
+++ routes.py
@@ -0,0 +1,78 @@
+from app import app
+from flask import render_template, request, redirect
+import reviews, users, restaurants
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/main")
+def main():
+    list = restaurants.get_list()
+    length = len(list)
+    return render_template("main.html", restaurants=list, length=length)
+
+@app.route("/list")
+def list():
+    selected_option = request.form.get("selectedOption")
+    list = reviews.get_list(selected_option)
+    return render_template("list.html", reviews=list)
+
+@app.route("/review", methods=["GET", "POST"])
+def review():
+    if request.method == "GET":
+        return render_template("review.html")
+    if request.method == "POST":
+        rating = request.form["rating"]
+        content = request.form["content"]
+        if reviews.send(restaurant_id, rating, content):
+            return redirect("/main")
+        else:
+            return render_template("error.html", message="Ravintolan lisäys ei onnistunut")
+        
+@app.route("/new", methods=["GET", "POST"])
+def new():
+    if request.method == "GET":
+        return render_template("new.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        longitude = request.form["longitude"]
+        latitude = request.form["latitude"]
+        if restaurants.add_restaurant(name, longitude, latitude):
+            return redirect("/main")
+        else:
+            return render_template("error.html", message="Ravintolan lisäys ei onnistunut")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/main")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
