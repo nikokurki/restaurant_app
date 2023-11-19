2b562e74d2e7496963acd68d0e46e438e959ec27
Niko <101094386+nikokurki@users.noreply.github.com>
2023-11-20 00:39:25 +0200

    Add files via upload

--- reviews.py
+++ reviews.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql import text
 import users, restaurants
 
 def get_list(restaurant_id):
-    sql = text("SELECT content, rating, user_id, sent_at FROM ratings WHERE restaurant_id=restaurant_id ORDER BY rating_id")
+    sql = text("SELECT R.content, R.rating, U.username, R.sent_at FROM ratings R, users U WHERE R.restaurant_id=restaurant_id ORDER BY R.rating_id")
     result = db.session.execute(sql, {"restaurant_id":restaurant_id})
     return result.fetchall()
 
@@ -13,7 +13,7 @@ def send(restaurant_id, content, rating):
     if user_id == 0:
         return False
     sql = text("INSERT INTO ratings (restaurant_id, content, rating, user_id, sent_at) VALUES (:restaurant_id, :content, :rating, :user_id, NOW())")
-    db.session.execute(sql, {"restaurant_id":restaurant_id, "rating":rating, "content":content, "user_id":user_id})
+    db.session.execute(sql, {"restaurant_id":restaurant_id, "rating":rating, "content":rating, "user_id":user_id})
     db.session.commit()
     return True
 
