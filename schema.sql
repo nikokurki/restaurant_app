8761b8e44fe90e384684fb1874e2510e338b2606
Niko <101094386+nikokurki@users.noreply.github.com>
2023-11-27 13:10:37 +0200

    Add files via upload
    Added a new table.

--- schema.sql
+++ schema.sql
@@ -3,26 +3,32 @@ CREATE TABLE users (
 	username TEXT UNIQUE,
 	password TEXT
 );
+CREATE TABLE restaurants (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	longitude FLOAT,
+	latitude FLOAT
+);
 CREATE TABLE ratings ( 
-	rating_id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	restaurant_id INTEGER REFERENCES restaurants, 
 	content TEXT, 
 	rating INTEGER, 
 	user_id INTEGER REFERENCES users, 
 	sent_at TIMESTAMP
 );
-CREATE TABLE restaurants (
-	restaurant_id SERIAL PRIMARY KEY,
-	name TEXT,
-	location FLOAT
-);
-
 CREATE TABLE info (
-	info_id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	restaurant_id INTEGER REFERENCES restaurants,
 	description TEXT,
 	open_hours TEXT
 );
+CREATE TABLE restaurant_groups (
+    id SERIAL PRIMARY KEY,
+	category TEXT UNIQUE,
+	restaurant_id INTEGER REFERENCES restaurants
+);
+	
 	
 
 	
